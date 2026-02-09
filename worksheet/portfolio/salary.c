753a3c14e6d9dfbe9595a121a7fd7dfc7e88f4d2
Bahaeddine Dayya <161286313+fluffychilidogz@users.noreply.github.com>
2026-02-09 12:56:32 +0000

    ..

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,15 +3,19 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Bahaeddine Dayya
+ * ID: 202003508
  */
 
- int main( void ) {
-
     // define and initialise variables for the problem data 
+   
+   int salary = 36250;
+   float national_insurance = 0.08;
+   float tax_percent;
 
     // calculate the deductions and final take-home salary
+
+
 
     // Use only these print statement with appropriate formatting and variable names
     //printf("Salary £...",var_name);
